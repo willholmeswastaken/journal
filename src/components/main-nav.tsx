ce3f3d1cecf0bc68e8cd36986b66629b3135c3ab
Will Holmes <willholmes97@icloud.com>
2023-09-11 18:37:35 +0100

    changed nav look

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -9,39 +9,41 @@ export function MainNav({
 }: React.HTMLAttributes<HTMLElement>) {
     return (
         <nav
-            className={cn("hidden md:flex gap-6 md:gap-10 justify-between w-full items-center", className)}
+            className={cn("hidden md:flex justify-between w-full items-center", className)}
             {...props}
         >
-            <Link
-                href="/examples/dashboard"
-                className="text-sm font-bold transition-colors hover:text-primary inline-flex items-center justify-center"
-            >
-                Journal
-            </Link>
-            <Link
-                href="/examples/dashboard"
-                className="text-sm font-medium transition-colors hover:text-primary"
-            >
-                Overview
-            </Link>
-            <Link
-                href="/examples/dashboard"
-                className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-            >
-                Customers
-            </Link>
-            <Link
-                href="/examples/dashboard"
-                className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-            >
-                Products
-            </Link>
-            <Link
-                href="/examples/dashboard"
-                className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-            >
-                Settings
-            </Link>
+            <div className="flex flex-row gap-6 md:gap-10">
+                <Link
+                    href="/examples/dashboard"
+                    className="text-sm font-bold transition-colors hover:text-primary inline-flex items-center justify-center"
+                >
+                    Journal
+                </Link>
+                <Link
+                    href="/examples/dashboard"
+                    className="text-sm font-medium transition-colors hover:text-primary"
+                >
+                    Overview
+                </Link>
+                <Link
+                    href="/examples/dashboard"
+                    className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+                >
+                    Customers
+                </Link>
+                <Link
+                    href="/examples/dashboard"
+                    className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+                >
+                    Products
+                </Link>
+                <Link
+                    href="/examples/dashboard"
+                    className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+                >
+                    Settings
+                </Link>
+            </div>
             <UserNavOperations />
         </nav>
     )
